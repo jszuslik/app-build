fafc757b43bae1956acab5fed515f1bbca53c60f
Joshua Szuslik <josh@lanex.com>
2016-10-26 13:51:49 -0500

    Update

--- app-api/service/src/main/java/com/norulesweb/springapp/api/Application.java
+++ app-api/service/src/main/java/com/norulesweb/springapp/api/Application.java
@@ -1,9 +1,23 @@
 package com.norulesweb.springapp.api;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
 }
