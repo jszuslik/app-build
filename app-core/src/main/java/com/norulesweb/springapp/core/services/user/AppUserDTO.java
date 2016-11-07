b319958e0cce0458261cfef031966eabbfe81b88
Joshua Szuslik <joshuaszuslik@gmail.com>
2016-11-07 02:21:19 -0600

    Update

--- app-core/src/main/java/com/norulesweb/springapp/core/services/user/AppUserDTO.java
+++ app-core/src/main/java/com/norulesweb/springapp/core/services/user/AppUserDTO.java
@@ -1,6 +1,10 @@
 package com.norulesweb.springapp.core.services.user;
 
 import com.norulesweb.springapp.core.model.user.AppUser;
+import com.norulesweb.springapp.core.model.user.Authority;
+
+import java.util.Date;
+import java.util.List;
 
 public class AppUserDTO {
 
@@ -10,12 +14,30 @@ public class AppUserDTO {
 
 	protected String password;
 
+	protected String firstName;
+
+	protected String lastName;
+
+	protected String email;
+
+	protected Boolean enabled;
+
+	protected Date lastPasswordResetDate;
+
+	protected List<Authority> authorities;
+
 	public AppUserDTO() { }
 
 	public AppUserDTO(AppUser appUser) {
 		setId(appUser.getId());
 		setUserId(appUser.getUserId());
 		setPassword(appUser.getPassword());
+		setFirstName(appUser.getFirstName());
+		setLastName(appUser.getLastName());
+		setEmail(appUser.getEmail());
+		setEnabled(appUser.getEnabled());
+		setLastPasswordResetDate(appUser.getLastPasswordResetDate());
+		setAuthorities(appUser.getAuthorities());
 	}
 
 	public AppUser buildModel() {
@@ -23,6 +45,12 @@ public class AppUserDTO {
 		appUser.setId(getId());
 		appUser.setUserId(getUserId());
 		// Do no expose password here
+		appUser.setFirstName(getFirstName());
+		appUser.setLastName(getLastName());
+		appUser.setEmail(getEmail());
+		appUser.setEnabled(getEnabled());
+		appUser.setLastPasswordResetDate(getLastPasswordResetDate());
+		appUser.setAuthorities(getAuthorities());
 		return appUser;
 	}
 
@@ -50,4 +78,51 @@ public class AppUserDTO {
 		this.password = password;
 	}
 
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public Date getLastPasswordResetDate() {
+		return lastPasswordResetDate;
+	}
+
+	public void setLastPasswordResetDate(Date lastPasswordResetDate) {
+		this.lastPasswordResetDate = lastPasswordResetDate;
+	}
+
+	public List<Authority> getAuthorities() {
+		return authorities;
+	}
+
+	public void setAuthorities(List<Authority> authorities) {
+		this.authorities = authorities;
+	}
 }
